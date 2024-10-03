f280e6776ef3e73fc4eb05e97d426027c3a641d6
Kigo-P <peterkigo33@gmail.com>
2024-10-03 07:31:23 +0300

    used database migration

--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
+
 # contains definitions of tables and associated schema constructs
 metadata = MetaData()
 
@@ -19,3 +20,15 @@ class Employee(db.Model):
 
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.salary}>'
+
+# creating a model called department
+class Department(db.Model):
+    # creating a table called department
+    __tablename__ = "departments"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String)
+
+    # creating a string representation of the db
+    def __repr__(self):
+        return f"<Department {self.id}, {self.name}, {self.address}>"
